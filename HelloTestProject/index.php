e425f1cbbc14184f98b3b94c6c0119ba25fa3ada
ksurendrayadav <surendra.yadav@ascendiatech.in>
2015-01-28 16:50:05 +0530

    Update index.php

--- HelloTestProject/index.php
+++ HelloTestProject/index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php 
-        
+        // edited from git hub
         phpinfo();
         // put your code here
         ?>
